2c90aa29b230ba69885adc97401f2b14b74e1571
Takumi Yamamoto <2311140076g@ed.fuk.kindai.ac.jp>
2025-10-08 07:58:45 +0000

    kiso-1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   def L4
-    cookies[:c] ||= 0
-    cookies[:c] = cookies[:c].to_i + 1
-    render plain: cookies[:c]
+    session[:c] ||= 0
+    session[:c] = session[:c].to_i + 1
+    render plain: session[:c]
   end
 end
